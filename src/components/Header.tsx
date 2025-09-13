bf74cd4145e71f1e14d647138a3ff542378148e3
Gabe <contatogabemelo@gmail.com>
2025-09-13 12:15:29 -0300

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return ( 
+        <>
+        <div className="">
+            <h1 className="text-3xl font-bold underline">Coincap WS</h1>
+        </div>
+        </>
+     );
+}
+
+export default Header;
